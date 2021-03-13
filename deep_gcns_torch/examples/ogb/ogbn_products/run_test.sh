fc16fdb1ec766782d994721c60ff145eb10a3bf8
Hanson Lu <hansonlu.hl@gmail.com>
2021-03-13 11:12:16 -0800

    save logits

--- deep_gcns_torch/examples/ogb/ogbn_products/run_test.sh
+++ deep_gcns_torch/examples/ogb/ogbn_products/run_test.sh
@@ -2,4 +2,4 @@ export CUDA_VISIBLE_DEVICES=$2
 python test.py --self_loop --num_layers 14 --gcn_aggr softmax_sg --t 0.1 \
     --data_folder "dataset-$1/" \
     --eval_cluster_number 5 \
-    --model_load_path "checkpoints/random-train_10-full_batch_test_epoch$3.pth"
+    --model_load_path "checkpoints/random-train_10-full_batch_test_final.pth"
