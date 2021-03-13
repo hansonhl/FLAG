5199196824c54aa437fba53863b0465095a036ee
Hanson Lu <hansonlu.hl@gmail.com>
2021-03-13 11:32:56 -0800

    edit scripts

--- deep_gcns_torch/examples/ogb/ogbn_products/run_test.sh
+++ deep_gcns_torch/examples/ogb/ogbn_products/run_test.sh
@@ -3,3 +3,4 @@ python test.py --self_loop --num_layers 14 --gcn_aggr softmax_sg --t 0.1 \
     --data_folder "dataset-$1/" \
     --eval_cluster_number 5 \
     --model_load_path "checkpoints/random-train_10-full_batch_test_final.pth"
+# ./run_test.sh {hyperturing1|hyperturing2} 4
