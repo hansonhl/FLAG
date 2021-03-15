bug fix: output softmax instead of log_softmax; refactor

deep_gcns_torch/examples/ogb/ogbn_products/model.py
@@ -133,7 +133,10 @@ class DeeperGCN(torch.nn.Module):
 
         h = self.node_pred_linear(h)
 
-        return torch.log_softmax(h, dim=-1)
+        if self.gcns.training():
+            return torch.log_softmax(h, dim=-1)
+        else:
+            return torch.log_softmax(h, dim=-1), torch.softmax(h, dim=-1)
 
     def print_params(self, epoch=None, final=False):
 